74f0ab78a39a57ca5263d1e6c1c91192df182cdf
Dongsu Park <dongsu@kinvolk.io>
2019-01-29 20:04:17 +0100

    store log file to /var/log/tinc.log

--- service/tinc/process.sh
+++ service/tinc/process.sh
@@ -3,4 +3,4 @@
 mkdir --parents ${CONTAINER_STATE_DIR}
 touch ${CONTAINER_STATE_DIR}/docker-tinc-first-start-done
 
-exec tinc --config=${CONTAINER_SERVICE_DIR}/tinc/data start --bypass-security --no-detach ${TINC_CMD_ARGS}
+exec tinc --config=${CONTAINER_SERVICE_DIR}/tinc/data start --bypass-security --logfile=/var/log/tinc.log --no-detach ${TINC_CMD_ARGS}
